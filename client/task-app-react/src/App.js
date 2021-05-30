63049e3b283fac853520514da2522c8bfbcfff47
albyzyx <albinarun24@gmail.com>
2021-05-30 13:57:09 +0530

    implemented new user registration

--- client/task-app-react/src/App.js
+++ client/task-app-react/src/App.js
@@ -1,50 +1,77 @@
 import { useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useHistory,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Auth from "./infrastructure/AuthFacade";
 import LoginForm from "./presentation/components/LoginForm";
 import RegisterForm from "./presentation/components/RegisterForm";
 import HomePage from "./presentation/components/HomePage";
+import LoadingScreen from "./presentation/components/LoadingScreen";
 
 function App() {
   const mAuth = new Auth();
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    mAuth.init();
-  });
-
-  const [user, setuser] = useState({
-    uId: 0,
-  });
+    const currUser = mAuth.init();
+    if (currUser && !user) {
+      const foundUser = JSON.parse(currUser);
+      setUser(foundUser);
+    }
+    loading && setLoading(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const loginWithEmail = (email, password) => {
     let res = mAuth.loginWithEmail(email, password);
+
+    res.match({
+      userAuthenticated: (props) => {
+        setUser(props.user);
+      },
+    });
   };
 
   const registerNewUser = (name, email, password) => {
     let res = mAuth.registerNewUser(name, email, password);
+    res.match({
+      userAuthenticated: (props) => {
+        setUser(props.user);
+      },
+    });
+  };
+  const logoutUser = () => {
+    mAuth.logoutCurrentUser();
+    setUser();
   };
 
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/login">
-            <LoginForm onLogin={loginWithEmail} />
-          </Route>
           <Route path="/register">
             <RegisterForm onRegister={registerNewUser} />
           </Route>
+          <Route path="/login">
+            <LoginForm onLogin={loginWithEmail} />
+          </Route>
+
           <Route path="/">
-            <HomePage />
+            <SplashScreen
+              loading={loading}
+              user={user}
+              logoutUser={logoutUser}
+            />
           </Route>
         </Switch>
       </Router>
     </div>
   );
 }
+
+const SplashScreen = ({ loading, user, logoutUser }) => {
+  if (loading) {
+    return <LoadingScreen />;
+  }
+  return <HomePage user={user} onLogout={logoutUser} />;
+};
 
 export default App;
--- client/task-app-react/src/infrastructure/AuthFacade.js
+++ client/task-app-react/src/infrastructure/AuthFacade.js
@@ -10,21 +10,40 @@ class Auth {
   Auth() {}
   init() {
     //Initialises authfacade(Checks if the user is already logged in or not)
-    console.log("mAuth init");
+    const user = localStorage.getItem("user");
+    return user;
   }
   registerNewUser(name, email, password) {
     //mock call to auth api, returns either true or error(email already exist)
+    let res = {
+      user: {
+        uID: "78dsa9f7",
+        fullName: name,
+        email: email,
+        token: "1sdaf6s54f68sd1ngb5",
+      },
+    };
+    localStorage.setItem("user", JSON.stringify(res.user));
+    return AuthResponse.userAuthenticated(res);
   }
 
   loginWithEmail(email, password) {
     // mock call to auth api, returns either true or error(invalid cred)
-    let res;
-    if (password === "123") {
-      res = AuthResponse.invalidCredentials();
-    } else {
-      res = AuthResponse.userAuthenticated({ uID: "78dsa9f7" });
-    }
-    return res;
+    let res = {
+      user: {
+        uID: "78dsa9f7",
+        fullName: "Alby",
+        email: email,
+        token: "1sdaf6s54f68sd1ngb5",
+      },
+    };
+    console.log(res.user);
+    localStorage.setItem("user", JSON.stringify(res.user));
+    return AuthResponse.userAuthenticated(res);
+  }
+
+  logoutCurrentUser() {
+    localStorage.removeItem("user");
   }
 }
 
